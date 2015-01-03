d8270071f8eb9837a604e039c72768b824a3e62b
Buddy zhang <514981221@qq.com>
2015-01-03 16:00:20 +0800

    The sample for JNI

--- android/user/HAL/Sample_code/sample_JNI/TestJni.java
+++ android/user/HAL/Sample_code/sample_JNI/TestJni.java
@@ -1,8 +1,14 @@
 import java.util.*;
 public class TestJni {
+	/*
+	 * c interface for Java.
+	 */
 	public native void buddy();
 	static {
 		try{
+			/*
+			 * Load C lib *.so
+			 */
 			System.loadLibrary("TestJni");
 		}catch(Throwable e) {
 			System.out.println("The Load problem");
